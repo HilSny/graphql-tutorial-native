bbacabeb4d3a30348f0b45131e9c45acc3cde18c
Hillary Snyder <hillary.snyder5@gmail.com>
2017-09-21 09:08:11 -0500

    Add loading condition and add shows mapper

--- src/App.js
+++ src/App.js
@@ -14,9 +14,20 @@ const showQuery = gql`
 `
 class App extends Component {
   render() {
+    const {data} = this.props
+
+    if (data.loading) {
+      return <View />
+    }
+
     return (
       <View style={{alignItems: 'center', justifyContent: 'center', height: '100%'}}>
-        <Text>Hello world</Text>
+        {data.shows.map(show => (
+          <View key={show.id}>
+            <Text>{show.title}</Text>
+          </View>
+
+        ))}
       </View>
     )
   }
