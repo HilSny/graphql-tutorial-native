bb789d377051ba253e0555b0536db24c3800c90d
Hillary Snyder <hillary.snyder5@gmail.com>
2017-09-21 09:00:30 -0500

    Add query and wrap App in higher order component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react'
 import {Text, View} from 'react-native'
 
+import gql from 'graphql-tag'
+import {graphql} from 'react-apollo'
+
+const showQuery = gql`
+  query {
+    shows {
+      id
+      title
+    }
+  }
+`
 class App extends Component {
   render() {
     return (
       <View style={{alignItems: 'center', justifyContent: 'center', height: '100%'}}>
-        <Text>Hello world</Text> 
+        <Text>Hello world</Text>
       </View>
     )
   }
 }
 
-export default App
+export default graphql(showQuery)(App)
