a6dd7d6235c7ce56439ea26398cc129a8564d9b7
Hillary Snyder <hillary.snyder5@gmail.com>
2017-09-21 08:41:17 -0500

    Add apllo client and react-apollo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <View style={{alignItems: 'center', justifyContent: 'center', height: '100%'}}>
-        <Text>Hello world</Text>
+        <Text>Hello world</Text> 
       </View>
     )
   }
