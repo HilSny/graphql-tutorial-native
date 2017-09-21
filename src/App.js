17684eb51ef970f980ef0acb307061f1c971959b
Hillary Snyder <hillary.snyder5@gmail.com>
2017-09-21 08:39:43 -0500

    Create example app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={{alignItems: 'center', justifyContent: 'center', height: '100%'}}>
+      <Text>Hello world</Text>
+    </View>
+  )
+}
+
+export default App
---
+++ src/Main.js
@@ -0,0 +1,8 @@
+import App from './App'
+import React from 'react'
+
+const Main = () => (
+  <App />
+)
+
+export default Main
