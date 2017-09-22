9e8a6eec5cdd095ea763c4fe5b1c10c01164ff91
Hillary Snyder <hillary.snyder5@gmail.com>
2017-09-21 20:07:37 -0500

    Add apollo client and react apollo

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,17 @@
+import ApolloClient, {createNetworkInterface} from 'apollo-client'
+
+import {ApolloProvider} from 'react-apollo'
 import App from './App'
 import React from 'react'
 
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({uri: 'http://localhost:4000/graphql'})
+})
+
 const Main = () => (
-  <App />
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 )
 
 export default Main
